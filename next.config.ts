82a08916a38f780907204e2e15c7c8e3ec62a5e5
whyxuann <164989071+whyxuann@users.noreply.github.com>
2025-04-16 00:12:48 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/gradaide.github.io',
-  assetPrefix: '/gradaide.github.io/',
-  typescript: {
-    ignoreBuildErrors: true,  // ← 忽略 TS 錯誤
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // ← 忽略 ESLint 錯誤
-  },
+  output: 'export', // ✅ 這是現在的做法
 };
 
 export default nextConfig;
-
-
-
